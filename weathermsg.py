83b8049860461077a3ccff5494cc08316b609eea
willcbradley <willbradley22858@gmail.com>
2025-09-06 08:37:58 +0930

    Simplify setup w/ common variables

--- weathermsg.py
+++ weathermsg.py
@@ -2,11 +2,21 @@ import requests
 import json
 from datetime import datetime
 
-weather_key = "your-key"
+# Variables for setup
+
+weather_key = "add weatherAPI key here"
+your_lat = "add latitude here"
+your_lon = "add longitude here"
+pushover_token = "add pushover token here"
+pushover_user = "add pushover user here"
+your_first_name = "add your first name here"
+
+# Remainder of code below doesn't need to be touched for setup
+
 weather_url = "http://api.weatherapi.com/v1/forecast.json"
 weather_params = {
     'key': weather_key,
-    'q': "your-lat,your-lon"
+    'q': f"{your_lat},{your_lon}"
 }
 
 response = requests.get(weather_url, params=weather_params)
@@ -22,84 +32,6 @@ hours = []
 # Narrow Down To Today's Hours
 for object in data["forecast"]["forecastday"][0]["hour"]:
         hours.append(object)
-
-# # Arrays For UV Cutoffs
-uv_above_3 = []
-uv_above_5 = []
-uv_above_8 = []
-
-# Filter To Find All Times When UV Is Above 3, 5, and 8. Store In Corresponding Array.
-for object in hours:
-    if object["uv"] > 3:
-        uv_above_3_raw = int(object["time"][11:13])
-        if uv_above_3_raw >= 12:
-            uv_above_3_time = str(uv_above_3_raw - 12) + "pm"
-        else:
-            uv_above_3_time = str(uv_above_3_raw) + "am"
-        uv_above_3.append(uv_above_3_time)
-
-for object in hours:
-    if object["uv"] > 5:
-        uv_above_5_raw = int(object["time"][11:13])
-        if uv_above_5_raw >= 12:
-            uv_above_5_time = str(uv_above_5_raw - 12) + "pm"
-        else:
-            uv_above_5_time = str(uv_above_5_raw) + "am"
-        uv_above_5.append(uv_above_5_time)
-
-for object in hours:
-    if object["uv"] > 8:
-        uv_above_8_raw = int(object["time"][11:13])
-        if uv_above_8_raw >= 12:
-            uv_above_8_time = str(uv_above_8_raw - 12) + "pm"
-        else:
-            uv_above_8_time = str(uv_above_8_raw) + "am"
-        uv_above_8.append(uv_above_8_time)
-
-
-# Find Time 1hr After First And Last Item In Each Array (Since These Are The Only We Could Need To Access)
-if uv_above_3:
-    time_after_start_3 = str((int(uv_above_3[0][0:2])+1)) + uv_above_3[0][2:]
-    if len(uv_above_3) > 1:
-        time_after_end_3 = str((int(uv_above_3[-1][0:2])+1)) + uv_above_3[-1][2:]
-
-if uv_above_5:
-    time_after_start_5 = str((int(uv_above_5[0][0:2])+1)) + uv_above_5[0][2:]
-    if len(uv_above_5) > 1:
-        time_after_end_5 = str((int(uv_above_5[-1][0:2])+1)) + uv_above_5[-1][2:]
-
-if uv_above_8:
-    time_after_start_8 = str((int(uv_above_8[0][0:2])+1)) + uv_above_8[0][2:]
-    if len(uv_above_8) > 1:
-        time_after_end_8 = str((int(uv_above_8[-1][0:2])+1)) + uv_above_8[-1][2:]
-
-# Assign String To Each UV Cutoff, Removing Redundant Ones
-if uv_above_3:
-    if len(uv_above_3) == 1:
-        time_above_3 = f"The UV will exceed 3 between {uv_above_3[0]} and {time_after_start_3}"
-    else:
-        time_above_3 = f"The UV will exceed 3 between {uv_above_3[0]} and {time_after_end_3}"
-    if uv_above_5:
-        if len(uv_above_5) == 1:
-            time_above_5 = f", will exceed 5 between {uv_above_5[0]} and {time_after_start_5}"
-        else:
-            time_above_5 = f", will exceed 5 between {uv_above_5[0]} and {time_after_end_5}"
-        if uv_above_8:
-            if len(uv_above_8) == 1:
-                time_above_8 = f", and will exceed 8 between {uv_above_8[0]} and {time_after_start_8}"
-            else:
-                time_above_8 = f", will exceed 8 between {uv_above_8[0]} and {time_after_end_8}"  
-        else: time_above_8 = ", and won't exceed 8."
-    else:
-        time_above_5 = ", and won't exceed 5."
-        time_above_8=""
-else:
-    time_above_3 = "The UV will not exceed 3 today."
-    time_above_5 = ""
-    time_above_8 = ""
-
-# Output UV Findings
-uv_string = f"{time_above_3}{time_above_5}{time_above_8}"
 
 # Find Max Temp + Hour
 max_temp = -1000
@@ -127,15 +59,15 @@ rain_string = f"{data["forecast"]["forecastday"][0]["day"]["daily_chance_of_rain
 # print(rain_string)
 
 # All Together
-output = f"Good morning, your-name. {temp_string} {rain_string} {uv_string}"
+output = f"Good morning, {your_first_name}. {temp_string} {rain_string}"
 print(output)
 
 
 # Output To Iphone
 
 pushover_params = {
-    "token": "your-token",
-    "user": "your-user",
+    "token": f"{pushover_token}",
+    "user": f"{pushover_user}",
     "message": output
 }
 
